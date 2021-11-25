be3f056c85fc418e53f9c04a03ea3a99e174b0c6
IBM <thieuquangnghia2000@gmail.com>
2021-11-25 20:58:24 +0700

    retry

--- src/main/java/com/example/subproject_kttkpm_14btt_haibaotunghiahuy_department/service/DepartmentService.java
+++ src/main/java/com/example/subproject_kttkpm_14btt_haibaotunghiahuy_department/service/DepartmentService.java
@@ -4,7 +4,10 @@ import com.example.subproject_kttkpm_14btt_haibaotunghiahuy_department.entity.De
 import com.example.subproject_kttkpm_14btt_haibaotunghiahuy_department.entity.Student;
 import com.example.subproject_kttkpm_14btt_haibaotunghiahuy_department.repository.DepartmentRepository;
 import com.example.subproject_kttkpm_14btt_haibaotunghiahuy_department.vo.ResponseTemplateVO;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,7 +25,8 @@ public class DepartmentService {
         return departmentRepository.save(department);
     }
 
-    public ResponseTemplateVO getStudentWithDepartment(Long departmentId) {
+    @Retry(name= "basic", fallbackMethod = "fallbackRetry")
+    public ResponseEntity<ResponseTemplateVO> getStudentWithDepartment(Long departmentId) {
         ResponseTemplateVO vo = new ResponseTemplateVO();
         Department department = departmentRepository.findById(departmentId).get();
         vo.setDepartment(department);
@@ -31,6 +35,11 @@ public class DepartmentService {
                 Student.class);
 
         vo.setStudent(student);
-        return vo;
+        return  new ResponseEntity<ResponseTemplateVO>(vo, HttpStatus.OK);
+    }
+    public ResponseEntity <String> fallbackRetry(RuntimeException e){
+        System.out.print("error" + e.getMessage());
+        ResponseTemplateVO vo = new ResponseTemplateVO();
+        return new ResponseEntity <String>("khong tim thay du lieu",HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
