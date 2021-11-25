88485abdf965db82873ea0fdc84171259cbad296
IBM <thieuquangnghia2000@gmail.com>
2021-11-25 21:54:42 +0700

    ratelimiter

--- src/main/java/com/example/subproject_kttkpm_14btt_haibaotunghiahuy_department/service/DepartmentService.java
+++ src/main/java/com/example/subproject_kttkpm_14btt_haibaotunghiahuy_department/service/DepartmentService.java
@@ -4,6 +4,7 @@ import com.example.subproject_kttkpm_14btt_haibaotunghiahuy_department.entity.De
 import com.example.subproject_kttkpm_14btt_haibaotunghiahuy_department.entity.Student;
 import com.example.subproject_kttkpm_14btt_haibaotunghiahuy_department.repository.DepartmentRepository;
 import com.example.subproject_kttkpm_14btt_haibaotunghiahuy_department.vo.ResponseTemplateVO;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,7 +26,8 @@ public class DepartmentService {
         return departmentRepository.save(department);
     }
 
-    @Retry(name= "basic", fallbackMethod = "fallbackRetry")
+//    @Retry(name= "basic", fallbackMethod = "fallbackRetry")
+    @RateLimiter(name = "timeoutExample", fallbackMethod = "fallbackRatelimiter")
     public ResponseEntity<ResponseTemplateVO> getStudentWithDepartment(Long departmentId) {
         ResponseTemplateVO vo = new ResponseTemplateVO();
         Department department = departmentRepository.findById(departmentId).get();
@@ -40,6 +42,6 @@ public class DepartmentService {
     public ResponseEntity <String> fallbackRetry(RuntimeException e){
         System.out.print("error" + e.getMessage());
         ResponseTemplateVO vo = new ResponseTemplateVO();
-        return new ResponseEntity <String>("khong tim thay du lieu",HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity <String>("Ratelimiter chay thanh cong",HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
